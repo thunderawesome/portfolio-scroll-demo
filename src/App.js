43b80a0122b0dc61f199feae27bf0e6f3ddb53ef
thunderawesome <troyjsanders@gmail.com>
2022-11-04 12:19:47 -0500

    Fixed ambient light

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Scene } from './Scene'
-import { AmbientLight } from 'three'
 
 export default function App() {
   return (
     <Canvas gl={{ antialias: false }} frameloop="demand">
       <Suspense fallback={null}>
-        <AmbientLight />
+        <ambientLight intensity={0.5} />
         <Scene />
       </Suspense>
     </Canvas>
